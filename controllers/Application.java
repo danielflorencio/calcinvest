cb287c15e3c6c0aaa87055f7395fe49e2e88aff4
danielflorencio <danielflorencio20@gmail.com>
2022-09-29 05:01:03 -0300

    trynna upload old calcinvest

---
+++ controllers/Application.java
@@ -0,0 +1,30 @@
+package controllers;
+import play.*;
+import play.mvc.*;
+import java.util.*;
+import models.*;
+public class Application extends Controller {
+	// Redirecionadores de páginas meio termo
+    public static void index() {render();}
+    public static void home() {render();}
+    public static void calculadora() {render();}
+    public static void optfut() {render();}
+    public static void optprev() {render();}
+    public static void welcome() {render();}
+    public static void futValBruto() {render();}
+    public static void preValBruto() {render();}
+    public static void preValPercent() {render();}
+    public static void erroentrada() {render();}
+    
+    // Redirecionadores de formularios de calculo
+    public static void poupBruto() {render("Calculadora/poupBruto.html");}
+    public static void cdb() {render("Calculadora/cdb.html");}
+    public static void preFix() {render("Calculadora/preFix.html");}
+    public static void rendPrev() {render("Calculadora/rendPrev.html");}
+    public static void selicBruto() {render("Calculadora/selicBruto.html");}
+    public static void selicPerCent() {render("Calculadora/selicPerCent.html");}
+    public static void preCalcBruto() {render("Calculadora/preCalcBruto.html");}
+    public static void preCalcCent() {render("Calculadora/preCalcCent.html");}
+    
+    //public static void getback() {render("Calculadora" + )}
+}
